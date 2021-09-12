36e8cf8ab0c4387b2b99a4abda26b225c6ee57b1
Lindsey Dietz <diet0146@umn.edu>
2021-09-12 13:40:25 -0500

    ongoing updates to tutorial files

--- Tutorial2/buildingReco/scripts/fred_script.R
+++ Tutorial2/buildingReco/scripts/fred_script.R
@@ -1,46 +1,53 @@
-library(dplyr)
-library(fredr)
+#' @param start_date string in the form "YYYY-MM-DD"
+#' @param end_date string in the form "YYYY-MM-DD"
+#' @return data.frame
 
 fred_function <- function(start_date, end_date){
   
-  #Getting several different unemployment series
-  df1 <- fredr(
+  #------- Ensuring dplyr and fredr are loaded -----------------
+  library(dplyr)
+  library(fredr)
+  
+  #------ Getting several different unemployment series --------
+  df1 <- fredr::fredr(
     series_id = "UNRATE",
     observation_start = as.Date(start_date),
     observation_end = as.Date(end_date)) %>% 
-    mutate(series_id = " Total")
+    mutate(series_id = "Total")
   
-  df2 <- fredr(
+  df2 <- fredr::fredr(
     series_id = "LNS14000006",
     observation_start = as.Date(start_date),
     observation_end = as.Date(end_date)) %>% 
-    mutate(series_id = "Black/African American")
+    mutate(series_id = " Black/African American")
   
-  df3 <- fredr(
+  df3 <- fredr::fredr(
     series_id = "LNS14000003",
     observation_start = as.Date(start_date),
     observation_end = as.Date(end_date)) %>% 
-    mutate(series_id = "White")
+    mutate(series_id = " White")
   
-  df4 <- fredr(
+  df4 <- fredr::fredr(
     series_id = "LNS14000009",
     observation_start = as.Date(start_date),
     observation_end = as.Date(end_date)) %>% 
-    mutate(series_id = "Hispanic/Latino")
+    mutate(series_id = " Hispanic/Latino")
   
   
   df <- rbind(df1, df2, df3, df4) %>%
-    rename(Category = series_id,
-           Date = date,
-           UR = value) %>%
-    select(-realtime_start, -realtime_end) %>%
+    dplyr::rename(Category = series_id,
+                  Date = date,
+                  UR = value) %>%
+    dplyr::select(-realtime_start, -realtime_end) %>%
     #Adding recession indicators
-    mutate(recession = ifelse(Date >= '2020-02-01' | 
-                                (Date >= '2007-12-01' & Date < '2009-07-01') | 
-                                (Date >= '2001-03-01' & Date < '2001-12-01'), TRUE, FALSE),
-           quarter_start = ifelse(substr(Date, 6, 7) %in% c('01','04','07','10'), TRUE, FALSE))
+    dplyr::mutate(recession = ifelse(Date >= '2020-02-01' | 
+                                       (Date >= '2007-12-01' & Date < '2009-07-01') | 
+                                       (Date >= '2001-03-01' & Date < '2001-12-01'), TRUE, FALSE),
+                  quarter_start = ifelse(substr(Date, 6, 7) %in% c('01','04','07','10'), TRUE, FALSE))
   
+  #-------- Writing to disk for vintaging ------------------
   write.csv(df, paste0("../data/fred_out_", end_date, ".csv"), row.names = FALSE)
+  
   return(df)
   
 }
